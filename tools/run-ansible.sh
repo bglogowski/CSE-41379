ddc5808f3d8208f0fe5d35aff48f8ea4e63babcc
Bryan Glogowski <glogowsk@adobe.com>
2025-07-05 08:36:32 -0700

    uses terraform output

--- tools/run-ansible.sh
+++ tools/run-ansible.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
+#
+#ansible-playbook \
+#	-i ~/git/CSE-41379/ansible/inventory.ini \
+#	--user ec2-user \
+#	--private-key=~/.ssh/cse41379.pem \
+#	~/git/CSE-41379/ansible/setup_linux.yaml
+#
 
 ansible-playbook \
 	-i ~/git/CSE-41379/ansible/inventory.ini \
 	--user ec2-user \
 	--private-key=~/.ssh/cse41379.pem \
-	~/git/CSE-41379/ansible/setup_linux.yaml
-
-ansible-playbook \
-	-i ~/git/CSE-41379/ansible/inventory.ini \
-	--user ec2-user \
-	--private-key=~/.ssh/cse41379.pem \
+	--extra-vars "$( (cd ~/git/CSE-41379/terraform && terraform output --json ) | jq 'with_entries(.value |= .value)')" \
 	~/git/CSE-41379/ansible/install_jenkins.yaml
 
-ansible-playbook \
-	-i ~/git/CSE-41379/ansible/inventory.ini \
-	--user ec2-user \
-	--private-key=~/.ssh/cse41379.pem \
-	~/git/CSE-41379/ansible/install_executors.yaml
+#ansible-playbook \
+#	-i ~/git/CSE-41379/ansible/inventory.ini \
+#	--user ec2-user \
+#	--private-key=~/.ssh/cse41379.pem \
+#	~/git/CSE-41379/ansible/install_executors.yaml
+#
+#ansible-playbook \
+#	-i ~/git/CSE-41379/ansible/inventory.ini \
+#	--user ec2-user \
+#	--private-key=~/.ssh/cse41379.pem \
+#	~/git/CSE-41379/ansible/install_sonarqube.yaml
 
-ansible-playbook \
-	-i ~/git/CSE-41379/ansible/inventory.ini \
-	--user ec2-user \
-	--private-key=~/.ssh/cse41379.pem \
-	~/git/CSE-41379/ansible/install_sonarqube.yaml
+#ansible-playbook \
+#	-i ~/git/CSE-41379/ansible/inventory.ini \
+#	--user ec2-user \
+#	--private-key=~/.ssh/cse41379.pem \
+#	~/git/CSE-41379/ansible/install_nexus.yaml
+#
+#
+#
